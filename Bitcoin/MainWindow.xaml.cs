dc8cf8ebe64473adcffbe8bb5abd83aab4418cd1
Eketrin <Katebaiko@yandex.ru>
2023-12-11 17:12:19 +0300

    удаление элемента

--- Bitcoin/MainWindow.xaml.cs
+++ Bitcoin/MainWindow.xaml.cs
@@ -24,5 +24,94 @@ namespace Bitcoin
         {
             InitializeComponent();
         }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        Dictionary<string, int> sortirovka = new Dictionary<string, int>();
+        private void AddToList_Click(object sender, RoutedEventArgs e)
+        {
+
+            string text = Input.Text;
+            if (text != "")
+            {
+                List.Items.Add(text);
+
+                if (sortirovka.ContainsKey(text)) //если есть
+                {
+                    sortirovka[text] = (sortirovka[text]) + 1;
+                }
+                else 
+                {
+                    sortirovka.Add(text, 1);
+                }
+            }
+        }
+        public string FirstL(string text)
+        {
+            string firstLetters = "";
+            foreach (var part in text.Split(' '))
+            {
+                firstLetters += part.Substring(0, 1).ToUpper();
+            }
+            return firstLetters;
+        }
+        private void DoResult_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (var item in sortirovka.OrderByDescending(x => x.Value))
+            {
+                if (FirstL(item.Key) == Search.Text)
+                {
+                    Result.Text += item.Key + " " + item.Value + "\n";
+                }
+                else continue;
+            }
+        }
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+        }
+
+        private void RemoveDay_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < List.SelectedItems.Count; i++)
+            {
+                foreach (var item in sortirovka)
+                {
+                    if (String.Equals(item.Key, List.SelectedItems[i].ToString()))
+                    {
+                        sortirovka[item.Key] = sortirovka[item.Key] - 1;
+                        if (item.Value <= 0)
+                        {
+                            sortirovka.Remove(item.Key);
+                        }
+                        break;
+                    }
+                }
+                List.Items.Remove(List.SelectedItems[i]);
+
+                //sortirovka[text] = (sortirovka[text]) + 1;
+            }
+
+
+
+        }
+    
     }
 }
